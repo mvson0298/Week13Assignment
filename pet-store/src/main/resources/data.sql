b6383c59d65d4d3e315ea5ff610b5c92e43efdd8
mvson0298 <140126685+mvson0298@users.noreply.github.com>
2023-10-07 15:42:11 -0600

    first commit

---
+++ pet-store/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO customer (customer_first_name, customer_last_name, customer_email) 
+VALUES ('Mason', 'Curtis', 'mason0298@live.com');
+
+INSERT INTO employee (employee_first_name, employee_last_name, employee_phone, employee_job_title)
+VALUES ('Jeff', 'Hayes', 9991234567, 'Manager');
+
+INSERT INTO pet_store (pet_store_name, pet_store_address, pet_store_city, pet_store_state, pet_store_zip, pet_store_phone_number)
+VALUES ('PetMart', '123 W. Street', 'Boise', 'Idaho', 83704, 9877654321);
